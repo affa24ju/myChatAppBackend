b31a497523c6c0ea28b15c9dbd627e0a9101ca61
Fatema Afroza <fafroza2017@gmail.com>
2025-08-27 08:31:49 +0200

    First commit

---
+++ src/main/java/com/chatApp/myChatAppBackend/MyChatAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chatApp.myChatAppBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyChatAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyChatAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chatApp/myChatAppBackend/MyChatAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatApp.myChatAppBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyChatAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
