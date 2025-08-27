5e4bc570f4b9bc28a84df09fd7f94053e085cb8e
Fatema Afroza <fafroza2017@gmail.com>
2025-08-27 09:42:02 +0200

    Skapat ChatController

---
+++ src/main/java/com/chatApp/myChatAppBackend/controller/ChatController.java
@@ -0,0 +1,12 @@
+package com.chatApp.myChatAppBackend.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    // Two methods: add user to chat and send message to chat
+    // addUser to inform all that a new user has joined
+    // sendMessage to send message to all users in the chat
+
+}
